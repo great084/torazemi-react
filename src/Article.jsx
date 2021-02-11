8adca8d9cc89e23445f161cf306a28ece3b45dea
great084 <great084@gmail.com>
2021-02-11 11:28:52 +0900

    微調整

--- src/Article.jsx
+++ src/Article.jsx
@@ -6,8 +6,10 @@ const Article = (props) => {
       <h2>{props.title}</h2>
       <label htmlFor="check">公開状態：</label>
       <input type="checkbox" checked={props.isPublished} id="check" onClick={() => props.toggle()} />
-      <input type="button" value="いいね" onClick={()=> props.countUp() } />
-      <p> {props.count} </p>
+      <div>
+        <input type="button" value="いいね" onClick={()=> props.countUp() } />
+        <p> {props.count} </p>
+      </div>
     </React.Fragment>
   )
 };
